83e8c50f9ad8852cad64375536ab0cbbd2943bc1
Cole Futch <colefutch@Coles-MacBook-Pro.local>
2019-09-19 11:50:25 -0400

    Updated typo in service provider

--- src/UIKit3Laravel6PresetServiceProvider.php
+++ src/UIKit3Laravel6PresetServiceProvider.php
@@ -17,12 +17,12 @@ class UIKit3Laravel6PresetServiceProvider extends ServiceProvider
         PresetCommand::macro('uikit3', function ($command) {
             $command->info('UIKit3 frontend scaffolding installed successfully.');
             $command->comment('Please run "npm install && npm run dev" to compile the new frontend scaffolding.');
-            UIKit3Preset::install();
+            UIKit3Laravel6Preset::install();
         });
 
         PresetCommand::macro('uikit3-auth', function ($command) {
             $command->info('UIKit3 frontend auth scaffolding installed successfully.');
-            UIKit3Preset::addAuthTemplates();
+            UIKit3Laravel6Preset::addAuthTemplates();
         });
     }
 }
